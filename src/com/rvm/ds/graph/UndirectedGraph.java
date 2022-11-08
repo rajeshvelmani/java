6cb8da011354d00131d1effafde595d2bb1a1327
rajeshvelmani <32807380+rajeshvelmani@users.noreply.github.com>
2022-11-08 23:59:15 +0530

    Update UndirectedGraph.java

--- src/com/rvm/ds/graph/UndirectedGraph.java
+++ src/com/rvm/ds/graph/UndirectedGraph.java
@@ -1,17 +1,58 @@
 package com.rvm.ds.graph;
 
+import java.util.LinkedList;
+
+/*
+ * 
+ */
 public class UndirectedGraph {
 
-    private final int[][] adjMatrix;
-    private final int nodes;
-    private final int edges;
+    private LinkedList<Integer>[] adjMatrix;
+    private int nodes;
+    private int edges;
 
     public UndirectedGraph(int nodes) {
         this.nodes = nodes;
         this.edges = 0;
-        this.adjMatrix = new int[nodes][nodes];
+        this.adjMatrix = new LinkedList[nodes];
+
+        for (int i = 0; i < nodes; i++) {
+            this.adjMatrix[i] = new LinkedList<>();
+        }
     }
 
+    public void addEdge(int nodeA, int nodeB) {
+        this.adjMatrix[nodeA].add(nodeB);
+        this.adjMatrix[nodeB].add(nodeA);
+        this.edges++;
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("nodes : ").append(this.nodes).append("\n");
+        sb.append("edges : ").append(this.edges).append("\n");
+
+        sb.append("\n");
+
+        for (int i = 0; i < this.nodes; i++) {
+            sb.append(" ==> ");
+            for (int j : this.adjMatrix[i]) {
+                sb.append(j).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        UndirectedGraph graph = new UndirectedGraph(4);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+
+        System.out.print(graph.toString());
+    }
 
 }
